e373ef40264aa2ae47f94b0679a4b795d5ecaacd
Qwertiush <63415593+Qwertiush@users.noreply.github.com>
2022-11-05 16:51:39 +0100

    Added firebase

--- pim_smarket/lib/main.dart
+++ pim_smarket/lib/main.dart
@@ -10,6 +10,7 @@ import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  //Firebase
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
